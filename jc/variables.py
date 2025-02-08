c1a835204a377e0cf35d22516645c100423e9646
abdeljamil <abdeljamil610@gmail.com>
2025-02-08 18:29:05 +0100

    first commit

---
+++ jc/variables.py
@@ -0,0 +1,80 @@
+#coding:utf-8
+
+"""
+Nommer une variable : doit commencer par une lettre 
+                      ne pas contenir de caractère spèciaux 
+                      ne pas contenir d'espaces
+                      utiliser des underscore(_)  
+
+Type standards      :entier numerique(int)
+                    nombre flottant(float)
+                    chaîne de caractères (str)
+                    booléen(bool)   
+                    autres(listes,dictionnaire,tuplet,etc) 
+
+Fonctions vues :    print()     ->afficher à l'écran
+                    input()     -> lire au clavier
+                    type()      -> retourner le type d'une donnée,variable,etc.
+                    int(),float(),str(),bool()->"caster" une donnée,la convertir.
+                    str.format() -> formater une chaîne       
+"""
+
+"""
+agePersonne
+age_personne
+AgePersonne
+Age_Personne
+_AgePersonne
+""" 
+
+
+# agePersonne = 14
+#agePersonne = 0XFAD5B8
+#agePersonne = 0705
+# prixHT = 12.46
+# agePersonne2 = "25"
+# continuer_partie = True
+
+
+# print("Age de la personne",agePersonne,".")
+# print(agePersonne)
+# print("agePersonne")
+# print(type(agePersonne))
+# print(type(agePersonne2))
+
+# texte = "l'âge de la personne est {} et le prix Ht est de {} euro"
+
+# print((texte).format(agePersonne,prixHT ))
+# print(texte.format(agePersonne,prixHT ))
+# print("l'âge de la personne est {} et le prix Ht est de {} euro.".format(agePersonne,prixHT ))
+
+
+# nomJoueur = "Diablo"
+
+# print("Bienvenue,",nomJoueur)
+
+
+# nomJoueur = input("Choisissez un nom du joueur :")
+
+# print("Bienvenue,",nomJoueur)
+
+
+# prixHT = input("Entrez un prix HT : ")
+# prixHT = int (prixHT)
+
+# prixTTC = prixHT + ( prixHT * 20 / 100)
+
+# print("Prix TTC =",prixTTC)
+
+
+# valeur_booleen = True
+
+# valeur_booleen =  int(valeur_booleen)
+
+# print(valeur_booleen)
+
+
+# valeur_booleen = False
+# valeur_booleen =  str(valeur_booleen)
+# print(valeur_booleen)
+
