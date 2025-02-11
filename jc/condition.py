d5a1c65edfad9ebf0ec3938232114ee9f79f792a
abdeljamil <abdeljamil610@gmail.com>
2025-02-11 18:02:56 +0100

    màj

--- jc/condition.py
+++ jc/condition.py
@@ -1,3 +1,17 @@
+"""
+Operateurs de comparaison : == (égal à)
+                            != (difference de)
+                            < (plus petit que)
+                            > (plus grand que)
+                            <= (plus petit ou égal à)
+                            >=(plus grand ou égal à)
+"""
+
+
+
+
+
+
 identifiant = "jason"
 mot_de_passe = "passeword123"
 
@@ -6,4 +20,6 @@ print("Interface de connexion")
 user_id = input("Entrez votre identifiant : ")
 user_password = input("Entrez votre mot de passe : ")
 
-print("Vous êtes connectés,Bienvenu",user_id) 
+if user_password == mot_de_passe:
+    print("Vous êtes connectés,Bienvenu",user_id) 
+
