dd665ef472d7136112ace37d77375930e82067e4
abdeljamil <abdeljamil610@gmail.com>
2025-02-11 18:37:22 +0100

    mj

--- jc/condition.py
+++ jc/condition.py
@@ -5,6 +5,11 @@ Operateurs de comparaison : == (égal à)
                             > (plus grand que)
                             <= (plus petit ou égal à)
                             >=(plus grand ou égal à)
+
+
+condition multiples     : and(ET)
+                        : or(OU)
+                        : in/not in (DANS / PAS DANS)                       
 """
 
 
@@ -12,14 +17,19 @@ Operateurs de comparaison : == (égal à)
 
 
 
-identifiant = "jason"
-mot_de_passe = "passeword123"
+# identifiant = "jason"
+# mot_de_passe = "passeword123"
 
-print("Interface de connexion")
+# print("Interface de connexion")
 
-user_id = input("Entrez votre identifiant : ")
-user_password = input("Entrez votre mot de passe : ")
+# user_id = input("Entrez votre identifiant : ")
+# user_password = input("Entrez votre mot de passe : ")
 
-if user_password == mot_de_passe:
-    print("Vous êtes connectés,Bienvenu",user_id) 
+# if user_id== identifiant and user_password == mot_de_passe:
+#     print("Vous êtes connectés,Bienvenu",user_id) 
+# print("je ne suis pas dans le 'if'")
 
+
+lettre_hasard = "b"
+if lettre_hasard not in 'aeiouy':
+    print("c'est une consonne")
