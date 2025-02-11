40f063d9ebb1c7ea0cab0281417caa5f22d10fbe
abdeljamil <abdeljamil610@gmail.com>
2025-02-11 17:50:26 +0100

    mj

---
+++ jc/condition.py
@@ -0,0 +1,9 @@
+identifiant = "jason"
+mot_de_passe = "passeword123"
+
+print("Interface de connexion")
+
+user_id = input("Entrez votre identifiant : ")
+user_password = input("Entrez votre mot de passe : ")
+
+print("Vous êtes connectés,Bienvenu",user_id) 
