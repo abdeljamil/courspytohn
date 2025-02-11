3c9c1c90435d493343759b8094c02b6d00077170
abdeljamil <abdeljamil610@gmail.com>
2025-02-11 17:29:27 +0100

    mj

--- jc/operation.py
+++ jc/operation.py
@@ -5,7 +5,7 @@ Operations en python: +(additions)
                       -(soustractions)
                       *(mutiplications)
                       /(division)
-                      %(modulo)->reste d'une 
+                      %(modulo)->reste d'une division Euclidien
 """
 
 
@@ -18,13 +18,37 @@ Operations en python: +(additions)
 # print(calcul)
 
 
-# calcul = 5/2 
+# calcul = 5/2  
 # calcul = int(calcul)
 
 # print("Resultat =",calcul)
 
 
-calcul = 5/2 
-calcul = float(calcul)
+# calcul = 5/2 
+# calcul = float(calcul)
 
-print("Resultat =",calcul)
+# print("Resultat =",calcul) 
+
+# calcul = 14/2
+# calcul = int(calcul)
+# calcul2 = 5%2
+# calcul2 = int(calcul2)
+
+# print("Resultat =",calcul)
+ 
+# print("Résultat2 =",calcul2)
+
+
+# calcul = (5+3)*7
+
+# print(calcul)
+
+
+niveauPersonnage = input("Niveau de départ ?")
+niveauPersonnage = int(niveauPersonnage)
+
+print("Niveau du personnage",niveauPersonnage)
+print("Combat réussi, tu gagnes un niveau !")
+niveauPersonnage = niveauPersonnage + 1
+
+print("Niveau du personnage",niveauPersonnage)
