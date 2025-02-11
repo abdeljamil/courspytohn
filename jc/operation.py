7ec96a48a03e067ea0c570febf02386c8a47cc74
abdeljamil <abdeljamil610@gmail.com>
2025-02-11 13:34:20 +0100

    màj

---
+++ jc/operation.py
@@ -0,0 +1,15 @@
+#coding:utf-8
+
+"""
+Operations en python: +(additions)
+                      -(soustractions)
+                      *(mutiplications)
+                      /(division)
+                      %(modulo)->reste d'une 
+"""
+
+
+calcul = 5/2 
+calcul = int(calcul)
+
+print("Résultat =",calcul)
