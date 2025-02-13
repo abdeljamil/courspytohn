07984cdb1a3ae4ba9a9219e746f5dae7251758ec
abdeljamil <abdeljamil610@gmail.com>
2025-02-13 06:33:45 +0100

    mj

---
+++ jc/boucles.py
@@ -0,0 +1,55 @@
+#coding:utf-8
+
+
+"""
+Boucles :while/for
+Mots-clés :breack (casse la boucle)/continue(revient au début de la boucle)
+"""
+# print("Je suis une phrase")
+# print("Je suis une phrase")
+# print("Je suis une phrase")
+# print("Je suis une phrase")
+# print("Je suis une phrase")
+
+
+# compteur = 0
+
+# while compteur < 5:
+#     print("Je suis une phrase")
+#     compteur += 1
+
+
+# print("Je suis sorti de la boucle...")   
+
+
+# jeu_lance = True 
+# print("")
+# while jeu_lance:
+#     #fais des instructions en rapport avec le jeu
+#     choixMenu = input(">")
+
+#     if choixMenu == "again":
+#         continue
+#     elif choixMenu == "quit":
+#         break
+#     elif choixMenu == "hello":
+#         print("Bonjour :) !")
+#     else:
+#         print("commande introuvable")
+
+# print("A bientôt...")    
+
+
+# sentence = "Bonjour tout le monde :)!"
+ 
+# for letter in sentence:
+#     print(letter)
+# print("A bientôt....")
+
+
+
+list_news = get_new()
+
+for news in list_news:
+    print()
+print("A bientôt...") 
