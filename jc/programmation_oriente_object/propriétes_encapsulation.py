fad84fd748cdd40760c2ac34ba1b7b5d17930928
abdeljamil <abdeljamil610@gmail.com>
2025-02-23 02:39:02 +0100

    màj

---
+++ jc/programmation_oriente_object/propriétes_encapsulation.py
@@ -0,0 +1,14 @@
+#coding:utf-8
+
+"""
+Propriété : Maniére de manipuler/contrôler des attributs
+            principe d'encapsulation !
+"""
+
+
+class Humain:
+    def __init__(self,nom,age):
+        self.nom = nom
+        self.age = age
+
+#Programme principal 
