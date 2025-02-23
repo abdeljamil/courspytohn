ec11af013d84a731bfeafefabaed849381acbeb2
abdeljamil <abdeljamil610@gmail.com>
2025-02-23 03:54:40 +0100

    mise à jour du code

--- jc/programmation_oriente_object/propriétes_encapsulation.py
+++ jc/programmation_oriente_object/propriétes_encapsulation.py
@@ -6,17 +6,45 @@ Propriété : Maniére de manipuler/contrôler des attributs
 """
 
 
+# class Humain:
+#     def __init__(self,nom,age):
+#         print("Création d'un Humain....")
+#         self.nom = nom
+#         self._age = age
+#     def _getage(self):
+#         return self._age
+#     #property(<getter>,<setter>,<deleter>,<helper>)
+#     age = property(_getage)
+
+# #Programme principal 
+
+# h1 = Humain("Jason",26)
+
+# print(h1.age)
+# # h1.age = 14
+# # print(h1.age) 
+
+
 class Humain:
     def __init__(self,nom,age):
+        print("Création d'un Humain....")
         self.nom = nom
         self._age = age
-
+    def _getage(self):
+        return self._age
+    def _getage(self):
+        return self._age
+    def _setage(self,nouvel_age):
+        self._age = nouvel_age
     #property(<getter>,<setter>,<deleter>,<helper>)
+    age = property(_getage,_setage)
 
 #Programme principal 
 
 h1 = Humain("Jason",26)
 
 print(h1.age)
-h1.age = 14
-print(h1.age) 
+h1.age = 20
+print(h1.age)
+# h1.age = 14
+# print(h1.age) 
