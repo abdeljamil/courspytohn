c4801a0afaca7269abe783c1173ec8e91bc1fc7a
abdeljamil <abdeljamil610@gmail.com>
2025-04-11 16:09:31 +0200

    update a file

--- jc/chaîne_caractères.py
+++ jc/chaîne_caractères.py
@@ -2,7 +2,57 @@
 
 #classe str : string (chaîne de caractère )
 
+"""
+NOTES
+Une Méthode de la chaîne travaille sur une copie, et pas sur la chaîne elle-même.
+str.upper();str.lower();str.capitalize();str.title();
+str.center(<largeur>;<caractere_remplisage>)
+str.find(<chaine>;<debut>;<fin>)
+str.index(<chaine>;<debut>;<fin>)
+"""
 
-ma_chaine = "Bonjour tout le monde"
 
-ma_chaine = ma_chaine.upper() 
+# ma_chaine = "bonjour tout le monde"
+
+# ma_chaine = ma_chaine.upper() 
+# print(ma_chaine)
+
+
+# ma_chaine = ma_chaine.lower() 
+# print(ma_chaine)
+
+# ma_chaine = ma_chaine.capitalize() 
+# print(ma_chaine)
+
+# ma_chaine = ma_chaine.title() 
+# print(ma_chaine)
+
+
+
+# ch1 = "bonjour"
+# ch2 = ch1
+
+
+# print(ch1)
+# print(ch2)
+
+# ch1 = ch1.upper()
+
+# print(ch1)
+# print(ch2)
+
+
+
+
+# ma_chaine = "MonsuperProgramme"
+# print(ma_chaine)
+
+# ma_chaine = ma_chaine.center(50,"-")
+
+# print(ma_chaine)
+
+
+ma_chaine = "MonSuperProgramme"
+
+print(ma_chaine.find("Super"))
+
