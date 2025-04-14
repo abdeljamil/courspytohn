adaeaaa0df3a8eb274111e701fe5df7fdbd11df3
abdeljamil <abdeljamil610@gmail.com>
2025-04-14 16:23:27 +0200

    update a file

--- jc/tuples.py
+++ jc/tuples.py
@@ -1,5 +1,12 @@
 #utf-8
 
+"""
+Création de tuple : mon_tuple = () #vide 
+                    mon_tuple = 17, #une seule valeur 
+                    mon_tuple = (17,) #Idem qu'au-dessus 
+                    mon_tuple = (4,5) #Plusieurs   valeurs
+
+"""
 # liste = [1,2,3,4,5,6]
 
 # for chose in enumerate(liste):
@@ -10,5 +17,8 @@
 # print(type(mon_tuple))
 
 
-mon_tuple = (45)
-print(type(mon_tuple))
+# mon_tuple = (45)
+# print(type(mon_tuple))
+
+mon_tuple = (45,64)
+print(type(mon_tuple[1]))
