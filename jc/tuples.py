a5c2dc3deaffc83f600330b595bacec50d8f9042
abdeljamil <abdeljamil610@gmail.com>
2025-04-14 16:17:15 +0200

    update

--- jc/tuples.py
+++ jc/tuples.py
@@ -1,10 +1,14 @@
+#utf-8
+
 # liste = [1,2,3,4,5,6]
 
 # for chose in enumerate(liste):
 #     print(chose)
 
 
-mon_tuple = ()
+# mon_tuple = ()
+# print(type(mon_tuple))
 
-mon_tuple = ()
-print(type())
+
+mon_tuple = (45)
+print(type(mon_tuple))
