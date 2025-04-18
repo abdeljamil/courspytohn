25d2637930aa26229d6594aa08b1cfbe912c1f04
abdeljamil <abdeljamil610@gmail.com>
2025-04-18 20:15:04 +0200

    update a file

---
+++ jc/dictionnaires.py
@@ -0,0 +1,12 @@
+#coding:utf-8
+
+"""
+création de dictionnaire : dico = {} #vide
+                         : dico = {<cle>:<valeur>,<cle2>:<valeur>}
+"""
+# dico = {"prénom":"jacson"}
+# print(dico["prénom"])
+
+# dico = {1:"jacson"}
+# print(dico[1])
+
