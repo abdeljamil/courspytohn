bad11972132ddbe1de5a0f0c744ec6dd8c196831
abdeljamil <abdeljamil610@gmail.com>
2025-08-16 02:56:23 +0100

    update a file positionn..

--- jc/positionnement_widgets.py
+++ jc/positionnement_widgets.py
@@ -19,17 +19,402 @@
 
 
 
+# import tkinter
+
+# app = tkinter.Tk()
+# app.geometry("640x480")
+# app.title("Positionnement widgets")
+
+# #Widgets .....
+# mainframe = tkinter.LabelFrame(app,text="Titre",width=300,height=200,borderwidth=1)
+# mainframe.pack()
+
+# btn = tkinter.Button(app,text="BIENVENU")
+# btn.pack()
+# #Boucle principâle 
+# app.mainloop()
+
+
+
+
+# import tkinter
+
+# app = tkinter.Tk()
+# app.geometry("640x480")
+# app.title("Positionnement widgets")
+
+# #Widgets .....
+# label = tkinter.Label(app,text="Un label")
+# ent = tkinter.Entry(app)
+# btn = tkinter.Button(app,text="Titre"  )
+
+
+# label.pack(side="bottom")
+# ent.pack(side="bottom")
+# btn.pack(side="bottom")
+
+
+
+# btn = tkinter.Button(btn,text="BIENVENU")
+# btn.pack()
+# #Boucle principâle 
+# app.mainloop()
+
+
+
+
+
+
+
+
+# import tkinter
+
+# app = tkinter.Tk()
+# app.geometry("640x480")
+# app.title("Positionnement widgets")
+
+# #Widgets .....
+# label = tkinter.Label(app,text="Un label")
+# ent = tkinter.Entry(app)
+# btn = tkinter.Button(app,text="Titre"  )
+
+
+# label.pack()
+# ent.pack()
+# # btn.pack(expand=1)
+# # btn.pack(side="left",fill="y")
+# # btn.pack(fill="y")
+# # btn.pack(fill="both")
+# btn.pack(fill="none")
+
+
+# btn = tkinter.Button(btn,text="BIENVENU")
+# btn.pack()
+# #Boucle principâle 
+# app.mainloop()
+
+
+
+
+
+
+
+
+# import tkinter
+
+# app = tkinter.Tk()
+# app.title("Positionnement widgets")
+
+# #Widgets .....
+# label = tkinter.Label(app,text="Un label")
+# ent = tkinter.Entry(app)
+# btn = tkinter.Button(app,text="Titre"  )
+
+
+# label.pack()
+# ent.pack()
+# # btn.pack(expand=1)
+# # btn.pack(side="left",fill="y")
+# # btn.pack(fill="y")
+# # btn.pack(fill="both")
+# btn.pack(padx=100,pady=5)
+
+
+# btn = tkinter.Button(btn,text="BIENVENU")
+# btn.pack()
+# #Boucle principâle 
+# app.mainloop()
+
+
+
+
+
+
+
+
+# import tkinter
+
+# app = tkinter.Tk()
+# app.title("Positionnement widgets")
+
+# #Widgets .....
+# label = tkinter.Label(app,text="Un label")
+# ent = tkinter.Entry(app)
+# btn = tkinter.Button(app,text="Titre"  )
+
+
+# label.pack()
+# ent.pack()
+# # btn.pack(expand=1)
+# # btn.pack(side="left",fill="y")
+# # btn.pack(fill="y")
+# # btn.pack(fill="both")
+# btn.pack(ipadx=100,ipady=5)
+
+
+# btn = tkinter.Button(btn,text="BIENVENU")
+# btn.pack()
+# #Boucle principâle 
+# app.mainloop()
+
+
+
+
+
+
+
+# import tkinter
+
+# app = tkinter.Tk()
+# app.geometry("640x480")
+# app.title("Positionnement widgets")
+
+# #Widgets .....
+# label = tkinter.Label(app,text="Un label")
+# ent = tkinter.Entry(app)
+# btn = tkinter.Button(app,text="Titre"  )
+
+
+# label.pack()
+# ent.pack()
+# # btn.pack(expand=1)
+# # btn.pack(side="left",fill="y")
+# # btn.pack(fill="y")
+# # btn.pack(fill="both")
+# btn.pack(ipadx=100,ipady=50)
+
+
+# btn = tkinter.Button(btn,text="BIENVENU")
+# btn.pack()
+# #Boucle principâle 
+# app.mainloop()
+
+
+
+
+
+
+
+
+
+
+# import tkinter
+
+# app = tkinter.Tk()
+# app.geometry("640x480")
+# app.title("Positionnement widgets")
+
+# #Widgets .....
+# label = tkinter.Label(app,text="Un label")
+# ent = tkinter.Entry(app)
+# btn = tkinter.Button(app,text="Titre"  )
+
+
+# label.grid()
+# ent.grid()
+# btn.grid()
+
+
+# btn = tkinter.Button(btn,text="BIENVENU")
+# btn.pack()
+# #Boucle principâle 
+# app.mainloop()
+
+
+
+
+
+
+# import tkinter
+
+# app = tkinter.Tk()
+# app.geometry("640x480")
+# app.title("Positionnement widgets")
+
+# #Widgets .....
+# label = tkinter.Label(app,text="Un label")
+# ent = tkinter.Entry(app)
+# btn = tkinter.Button(app,text="Titre"  )
+
+
+# label.grid(row=0,column=0)
+# ent.grid(row=0,column=1)
+# btn.grid(row=0,column=2)
+
+
+# btn = tkinter.Button(btn,text="BIENVENU")
+# btn.pack()
+# #Boucle principâle 
+# app.mainloop()
+
+
+
+
+
+
+# import tkinter
+
+# app = tkinter.Tk()
+# app.geometry("640x480")
+# app.title("Positionnement widgets")
+
+# #Widgets .....
+# label = tkinter.Label(app,text="Un label")
+# ent = tkinter.Entry(app)
+# btn = tkinter.Button(app,text="Titre"  )
+
+
+# label.grid(row=0,column=0, columnspan=2)
+# ent.grid(row=0,column=2)
+# btn.grid(row=0,column=3)
+
+
+# btn = tkinter.Button(btn,text="BIENVENU")
+# btn.pack()
+# #Boucle principâle 
+# app.mainloop()
+
+
+
+
+
+
+
+# import tkinter
+
+# app = tkinter.Tk()
+# app.geometry("640x480")
+# app.title("Positionnement widgets")
+
+# #Widgets .....
+# label = tkinter.Label(app,text="Un label")
+# ent = tkinter.Entry(app)
+# btn = tkinter.Button(app,text="Titre"  )
+
+
+# label.grid(row=0,column=0, columnspan=2,rowspan=3)
+# ent.grid(row=0,column=2)
+# btn.grid(row=0,column=3)
+
+
+# btn = tkinter.Button(btn,text="BIENVENU")
+# btn.pack()
+# #Boucle principâle 
+# app.mainloop()
+
+
+
+# import tkinter
+
+# app = tkinter.Tk()
+# app.geometry("640x480")
+# app.title("Positionnement widgets")
+
+# #Widgets .....
+# label = tkinter.Label(app,text="Un label")
+# ent = tkinter.Entry(app)
+# btn = tkinter.Button(app,text="Titre"  )
+
+
+# label.grid(padx=15,pady=50)
+# ent.grid()
+# btn.grid()
+
+
+# btn = tkinter.Button(btn,text="BIENVENU")
+# btn.pack()
+# #Boucle principâle 
+# app.mainloop()
+
+
+
+
+
+# import tkinter
+
+# """
+# n = nord
+
+# s = sud
+
+# e = est
+
+# w = ouest
+
+# """
+
+# app = tkinter.Tk()
+# app.geometry("640x480")
+# app.title("Positionnement widgets")
+
+# #Widgets .....
+# label = tkinter.Label(app,text="Un label")
+# ent = tkinter.Entry(app)
+# btn = tkinter.Button(app,text="Titre"  )
+
+
+# label.grid(sticky="ne")
+# ent.grid()
+# btn.grid()
+
+
+# btn = tkinter.Button(btn,text="BIENVENU")
+# btn.pack()
+# #Boucle principâle 
+# app.mainloop()
+
+
+
+
+
+# import tkinter
+
+# """
+# n = nord
+
+# s = sud
+
+# e = est
+
+# w = ouest
+
+# """
+
+# app = tkinter.Tk()
+# app.geometry("640x480")
+# app.title("Positionnement widgets")
+
+# #Widgets .....
+# btn = tkinter.Button(app,text="BIENVENU"  )
+# btn.place(x=10,y=10)
+# #Boucle principâle 
+# app.mainloop()
+
+
+
+
+
+
+
 import tkinter
+
+"""
+n = nord
+
+s = sud
+
+e = est
+
+w = ouest
+
+"""
 
 app = tkinter.Tk()
 app.geometry("640x480")
 app.title("Positionnement widgets")
 
 #Widgets .....
-mainframe = tkinter.LabelFrame(app,text="Titre",width=300,height=200,borderwidth=1)
-mainframe.pack()
-
-btn = tkinter.Button(app,text="BIENVENU")
-btn.pack()
+btn = tkinter.Button(app,text="BIENVENU"  )
+btn.place(x=500,y=10)
 #Boucle principâle 
 app.mainloop()
